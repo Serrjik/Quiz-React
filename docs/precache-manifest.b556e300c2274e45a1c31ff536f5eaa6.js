a7489fb536234b9420382eda3ee1a9972b40bb00
Serrjik <uac.sarge@gmail.com>
2020-08-30 17:34:05 +0300

    made a build of the project

---
+++ docs/precache-manifest.b556e300c2274e45a1c31ff536f5eaa6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5f98c311530132615e2a67c9a03d5f16",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5c99a2fce009adbc6471",
+    "url": "/static/css/main.215913b0.chunk.css"
+  },
+  {
+    "revision": "df13ccd6e524ec3f79b5",
+    "url": "/static/js/2.6b34c09c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.6b34c09c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5c99a2fce009adbc6471",
+    "url": "/static/js/main.b408f60d.chunk.js"
+  },
+  {
+    "revision": "b6cb5b40f490f68e2648",
+    "url": "/static/js/runtime-main.712fc8ca.js"
+  }
+]);
